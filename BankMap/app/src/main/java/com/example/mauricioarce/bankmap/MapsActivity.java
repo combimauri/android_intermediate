fd70c6a73895b85e696970f6f652cb5582dc549d
Mauricio Arce <mauricio.arce.mat@gmail.com>
2015-08-21 22:21:18 -0400

    BankMap little bugs fixed

--- BankMap/app/src/main/java/com/example/mauricioarce/bankmap/MapsActivity.java
+++ BankMap/app/src/main/java/com/example/mauricioarce/bankmap/MapsActivity.java
@@ -8,7 +8,6 @@ import android.widget.TextView;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.parse.Parse;
